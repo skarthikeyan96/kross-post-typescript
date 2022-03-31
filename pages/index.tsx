395f4da352b84a3653791c9debaf15607e4fbb99
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-03-31 17:59:00 +0530

    Add prettier , eslint and husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react'
 
-
-
-
-
 const Home: NextPage = () => {
-  // @ts-ignore
-  useEffect(() => { import('tw-elements'); }, []);
-
+  useEffect(() => {
+    // @ts-ignore
+    import('tw-elements')
+  }, [])
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Kross post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <div className="bg-blue-100 rounded-lg py-5 px-6 mb-4 text-base text-blue-700 " role="alert">
+        <div
+          className="mb-4 rounded-lg bg-blue-100 py-5 px-6 text-base text-blue-700 "
+          role="alert"
+        >
           A simple primary alert - check it out!
         </div>
 
@@ -72,9 +72,6 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
-
-  
-
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('@tailwindcss/typography')
   ]
 }
